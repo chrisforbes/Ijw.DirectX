cebc093346f469a3ce86e4f5ed2a36c48ab752fe
chrisf <chrisf@993157c7-ee19-0410-b2c4-bb4e9862e678>
2007-10-04 18:40:42 +0000

    git-svn-id: svn://svn.ijw.co.nz/svn/Ijw.DirectX@1748 993157c7-ee19-0410-b2c4-bb4e9862e678

---
+++ Ijw.DirectX/AssemblyInfo.cpp
@@ -0,0 +1,17 @@
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute("Ijw.DirectX")];
+[assembly:AssemblyDescriptionAttribute("IJW DirectX Wrapper")];
+[assembly:AssemblyConfigurationAttribute("")];
+[assembly:AssemblyCompanyAttribute("IJW Software")];
+[assembly:AssemblyProductAttribute("BluntDx")];
+[assembly:AssemblyCopyrightAttribute("Copyright (c) IJW Software")];
+[assembly:AssemblyTrademarkAttribute("")];
+[assembly:AssemblyCultureAttribute("")];
+[assembly:ComVisible(false)];
+[assembly:CLSCompliantAttribute(true)];
+[assembly:SecurityPermission(SecurityAction::RequestMinimum, UnmanagedCode = true)];
