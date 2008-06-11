1aabc162a126bcb9f7331397a9375330f7f6c28f
chrisf <chrisf@993157c7-ee19-0410-b2c4-bb4e9862e678>
2008-06-11 00:16:26 +0000

    git-svn-id: svn://svn.ijw.co.nz/svn/Ijw.DirectX@1978 993157c7-ee19-0410-b2c4-bb4e9862e678

--- Ijw.DirectX/Texture.h
+++ Ijw.DirectX/Texture.h
@@ -5,12 +5,26 @@ namespace Ijw { namespace DirectX
 	public ref class Texture
 	{
 	private:
-		Texture( IDirect3DBaseTexture9* texture ) : texture(texture) {}
+		Texture( IDirect3DBaseTexture9* texture ) : texture(texture), surf(0) {}
 
 	internal:
 		IDirect3DBaseTexture9* texture;
+		IDirect3DSurface9* surf;
 
 	public:
+		IntPtr GetSurface()
+		{
+			if (!surf)
+			{
+				IDirect3DSurface9* _surf;
+				HRESULT hr = ((IDirect3DTexture9*)texture)->GetSurfaceLevel( 0, &_surf );
+				if (FAILED( hr ))
+					throw gcnew InvalidOperationException( "Cant get surface. Wrong texture type?" );
+				surf = _surf;
+			}
+			return IntPtr(surf);
+		}
+
 		static Texture^ Create( Stream^ stream, GraphicsDevice^ device )
 		{
 			stream->Position = 0;
@@ -41,6 +55,17 @@ namespace Ijw { namespace DirectX
 
 			if (FAILED( hr = D3DXCreateCubeTextureFromFileInMemory( device->device, p, data->Length, (IDirect3DCubeTexture9**)&tex ) ))
 				throw gcnew InvalidOperationException("Texture load failed");
+
+			return gcnew Texture( tex );
+		}
+
+		static Texture^ CreateRenderTarget( GraphicsDevice^ device, int width, int height )
+		{
+			HRESULT hr;
+			IDirect3DBaseTexture9* tex;
+			if (FAILED( hr = D3DXCreateTexture( device->device, width, height, 1, D3DUSAGE_RENDERTARGET, D3DFMT_R32F, D3DPOOL_DEFAULT,
+				(IDirect3DTexture9**) &tex ) ))
+				throw gcnew InvalidOperationException("Failed creating render target");
 
 			return gcnew Texture( tex );
 		}
